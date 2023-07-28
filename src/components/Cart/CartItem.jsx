39b364da7ba004bba0cffe6687a5e0ed0b18ee0a
aria198a <aria198a@gmail.com>
2023-07-28 14:09:17 +0800

    modified

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -4,33 +4,34 @@ import { ReactComponent as PlusBtn } from "../../icons/plus.svg"
 import { ReactComponent as MinusBtn } from "../../icons/minus.svg"
 
 
-function Product({id, name, price, image, count}) {
-    return (
-        <div className={styles.productContainer} id={id} data-count="0" data-price={price}>
-          <img className={styles.imgContainer} alt={name} src={image} />
-          <div className={styles.productInfo}>
-            <div className={styles.productName}>{name}</div>
-            <div className={styles.productControlContainer}>
-              <div className={styles.productControl}>
-                <MinusBtn className={styles.productAction } />
-                <span className={styles.productCount}>{count}</span>
-                <PlusBtn className={styles.productAction } />
-              </div>
+
+function Product({id, name, price, image, quantity}) {
+  return (
+      <div className={styles.productContainer} id={id} data-count="0" data-price={price}>
+        <img className={styles.imgContainer} alt={name} src={image} />
+        <div className={styles.productInfo}>
+          <div className={styles.productName}>{name}</div>
+          <div className={styles.productControlContainer}>
+            <div className={styles.productControl}>
+              <MinusBtn className={styles.productAction } />
+              <span className={styles.productCount}>{quantity}</span>
+              <PlusBtn className={styles.productAction } />
             </div>
-            <div className={styles.price}>${price}</div>
           </div>
+          <div className={styles.price}>${price}</div>
         </div>
-    )
-  }
-  
-  function CartItem() {
-    return (
-      <section className={styles.productList} data-total-price="0">
-        {listData.map(item => 
-          <Product {...item} key={item.id} />
-        )}
-      </section>
-    )
-  }
-  
-  export default CartItem
+      </div>
+  )
+}
+
+function CartItem() {
+  return (
+    <section className={styles.productList} data-total-price="0">
+      {listData.map(item => 
+        <Product {...item} key={item.id} />
+      )}
+    </section>
+  )
+}
+
+export default CartItem
--- src/components/Cart/listData.js
+++ src/components/Cart/listData.js
@@ -1,16 +1,16 @@
 export const listData = [
-    {
-      id: '1',
-      name: '貓咪罐罐',
-      img: 'https://picsum.photos/300/300?text=1',
-      price: 100,
-      quantity: 2,
-    },
-    {
-      id: '2',
-      name: '貓咪干干',
-      img: 'https://picsum.photos/300/300?text=2',
-      price: 200,
-      quantity: 1,
-    },
-  ]
+  {
+    id: '1',
+    name: '貓咪罐罐',
+    image: 'https://picsum.photos/300/300?text=1',
+    price: 100,
+    quantity: 2,
+  },
+  {
+    id: '2',
+    name: '貓咪干干',
+    image: 'https://picsum.photos/300/300?text=2',
+    price: 200,
+    quantity: 1,
+  },
+]
